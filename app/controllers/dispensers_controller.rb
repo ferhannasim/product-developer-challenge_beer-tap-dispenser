85c9cb5a7998f71f6012b2754f59843340064902
ferhannasim <ferhannasim@gmail.com>
2023-07-27 14:45:00 +0500

    updated dispenser controller

--- app/controllers/dispensers_controller.rb
+++ app/controllers/dispensers_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/dispensers_controller.rb
 class DispensersController < ApplicationController
-    before_action :authenticate_user, except: [:usage_details]
+    # before_action :authenticate_user, except: [:usage_details]
     before_action :set_dispenser, only: [:show, :update]
     before_action :check_admin, only: [:create]
   
@@ -31,30 +31,16 @@ class DispensersController < ApplicationController
     end
 
     def usage_details
-      @tap_events = TapEvent.all
-      total_cost = @tap_events.pluck(:price).compact.sum
-
-      total = 0
-      time_intervals = @tap_events.where.not(closed_at: nil).map do |event|
-        time_interval = event.closed_at - event.opened_at
-        total += time_interval
-        time_interval
-      end
+      result = Dispenser.calculate_total_price
 
       render json: {
-        total_cost: total_cost,
-        Dispenser_used: @tap_events.count,
-        "Total_time_spend(Hours, Min)": convert_to_time(time_intervals.sum),
-        "Total_time_spend(Seconds)": time_intervals.sum,
+        total_cost: result[:total_cost],
+        total_time_spend: result[:total_time_spend],
+        total_time_spend_formatted: result[:total_time_spend_formatted]
       }
     end
   
     private
-
-    def convert_to_time(seconds)
-      time = Time.at(seconds).utc
-      time.strftime("%H:%M:%S")
-    end
 
     def check_admin
       if !@current_user.admin?
--- app/models/dispenser.rb
+++ app/models/dispenser.rb
@@ -1,3 +1,24 @@
 class Dispenser < ApplicationRecord
     has_many :tap_events
+
+    def self.calculate_total_price
+        total_cost = TapEvent.where.not(price: nil).sum(:price)
+        closed_events = TapEvent.where.not(closed_at: nil).pluck(:opened_at, :closed_at)
+
+        total_time_spend = 0
+        closed_events.each do |opened_at, closed_at|
+          time_interval = closed_at - opened_at
+          total_time_spend += time_interval
+        end
+      
+        {
+          total_cost: total_cost,
+          total_time_spend: total_time_spend,
+          total_time_spend_formatted: convert_to_time(total_time_spend)
+        }
+      end
+      
+    def self.convert_to_time(seconds)
+        Time.at(seconds).utc.strftime("%H:%M:%S")
+    end
 end
