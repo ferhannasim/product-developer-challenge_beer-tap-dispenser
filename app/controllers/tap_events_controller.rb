730fe575b1d220c5a7b87afe11d3180c617579f6
ferhannasim <ferhannasim@gmail.com>
2023-07-27 13:53:51 +0500

    refactor tap events controller add scopes and method in model class

--- app/controllers/tap_events_controller.rb
+++ app/controllers/tap_events_controller.rb
@@ -13,9 +13,9 @@ class TapEventsController < ApplicationController
     end
   
     def update
-      @tap_event = @dispenser.tap_events.find_by(status: 'open', closed_at: nil)
+      @tap_event = TapEvent.find_open_tap_dispenser(@dispenser.id)
       if @tap_event.update(closed_at: Time.now, status: 'closed')
-        calculated_price = calculate_price(@tap_event.opened_at, @tap_event.closed_at, @dispenser.flow_volume)
+        calculated_price = @tap_event.calculate_price(@tap_event.opened_at, @tap_event.closed_at, @dispenser.flow_volume)
         @tap_event.update(price: calculated_price)
         render json: @tap_event
       else
@@ -24,40 +24,23 @@ class TapEventsController < ApplicationController
     end
   
     def usage_details
-      @tap_events = @dispenser.tap_events.where(id: @dispenser.id)
+      @tap_events = TapEvent.find_tap_events_dispenser(@dispenser.id)
       total_cost = @tap_events.pluck(:price).compact.sum
 
-      total = 0
-      time_intervals = @tap_events.where.not(closed_at: nil).map do |event|
-        time_interval = event.closed_at - event.opened_at
-        total += time_interval
-        time_interval
-      end
+      time_intervals = @tap_events.get_time_intervals
 
       render json: {
         total_cost: total_cost,
-        Dispenser_used: @tap_events.count,
-        "Total_time_spend(Hours, Min)": convert_to_time(time_intervals.sum),
-        "Total_time_spend(Seconds)": time_intervals.sum,
+        dispenser_used: @tap_events.count,
+        "total_time_spend_in_hours": @tap_events.convert_to_time(time_intervals.sum),
+        "total_time_spend_in_seconds": time_intervals.sum,
       }
     end
   
     private
 
-    def convert_to_time(seconds)
-      time = Time.at(seconds).utc
-      time.strftime("%H:%M:%S")
-    end
-  
     def set_dispenser
       @dispenser = Dispenser.find(params[:dispenser_id])
-    end
-  
-    def calculate_price(start_time, end_time, flow_volume)
-      price_per_liter = 10  
-      liters_poured = (end_time - start_time) * flow_volume
-      price = liters_poured * price_per_liter
-      return price
-    end
+    end 
   end
   
--- app/models/tap_event.rb
+++ app/models/tap_event.rb
@@ -1,4 +1,29 @@
 class TapEvent < ApplicationRecord
   belongs_to :dispenser
   belongs_to :user
+
+  scope :find_open_tap_dispenser, ->(dispenser_id) { find_by(status: 'open', closed_at: nil, dispenser_id: dispenser_id) }
+
+  scope :find_tap_events_dispenser, ->(dispenser_id) { where(id: dispenser_id) }
+
+  def self.convert_to_time(seconds)
+    time = Time.at(seconds).utc
+    time.strftime("%H:%M:%S")
+  end
+  
+  def calculate_price(start_time, end_time, flow_volume)
+    price_per_liter = 10  
+    liters_poured = (end_time - start_time) * flow_volume
+    price = liters_poured * price_per_liter
+    return price
+  end
+
+  def self.get_time_intervals
+    total = 0
+    where.not(closed_at: nil).map do |event|
+      time_interval = event.closed_at - event.opened_at
+      total += time_interval
+      time_interval
+    end
+  end
 end
